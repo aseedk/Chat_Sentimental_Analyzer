e0173048938d159cfad5052c031188e3cae23feb
Aseed Khokhar <71330292+aseedk@users.noreply.github.com>
2021-05-24 11:14:44 +0500

    Project Chat

---
+++ WebApplication/ChatWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDBTest
+{
+    public partial class ChatWindow : Window
+    {
+        public string Username { get; set; }
+        public ChatWindow(string username)
+        {
+            InitializeComponent();
+            Welcome.Content = "Hello " + username;
+            MongoHelper.ConnectToMongoService();
+            var collections = MongoHelper.Database.GetCollection<BsonDocument>("Users");
+            var documents = collections.Find(new BsonDocument()).ToList();
+            foreach (BsonDocument doc in documents)
+            {
+                FriendsList.Items.Add(doc[1]+ " " + doc[2]);
+            }
+
+        }
+
+        
+    }
+}
